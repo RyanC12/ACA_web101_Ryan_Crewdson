b3b6c77a05377ba0478264b74909391beac0c517
unknown <rmi11s@yahoo.com>
2019-05-16 16:55:17 -0500

    buttons

---
+++ src/scripts/ticTacToe.js
@@ -0,0 +1,11 @@
+function changeMarkerToX() {
+	var gameMarker = "X";
+	console.log("The x button was clicked!");
+}
+function changeMarkerToO() {
+	var gameMarker = "0";
+	console.log("The x button was clicked!");
+}
+function placeMark() {
+	console.log("dsf");
+}
