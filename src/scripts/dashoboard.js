75e48de11b9dc287de47352ce76570bacf2b8a00
unknown <rmi11s@yahoo.com>
2019-05-17 23:55:23 -0500

    finished product

--- src/scripts/dashoboard.js
+++ src/scripts/dashoboard.js
@@ -1,16 +1,23 @@
-$(function() {
-	let $list = $("ul");
-	let $newItemForm = $("#newItemForm");
+// Append input value to new li
+$("input[name='listItem']").on("keypress", function(event) {
+	if (event.which === 13) {
+		let todoText = $(this).val();
+		$("ul").append("<li class='item'>" + todoText + "</li>");
+		console.log(todoText);
+		$("input[name='listItem']").val("");
+	}
+});
+// Append input with click button
+$("#add").click(function() {
+	let todoText = $("input[name='listItem']").val();
+	$("ul").append("<li class='item'>" + todoText + "</li>");
+	console.log(todoText);
+	$("input[name='listItem']").val("");
+});
 
-	$newItemForm.on("submit", function(e) {
-		e.preventDefault();
-		let text = $('input[type="text"]').val();
-		$list.append(`<li>${text}</li>`);
-		$('input[type="text"]').val("");
-	});
-
-	$list.on("click", "li", function() {
-		let $this = $(this);
-		$this.remove();
+// remove li when clicked on with a fadeout of half one second
+$("ul").on("click", "li", function() {
+	$(this).fadeOut(500, function() {
+		$(this).remove();
 	});
 });
