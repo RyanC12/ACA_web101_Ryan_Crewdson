4fe312547f69929696a74e43d9e7bfe344449e3c
unknown <rmi11s@yahoo.com>
2019-05-20 21:22:56 -0500

    reset button

--- src/scripts/ticTacToe.js
+++ src/scripts/ticTacToe.js
@@ -1,11 +1,22 @@
-function changeMarkerToX() {
-	var gameMarker = "X";
-	console.log("The x button was clicked!");
+let toggle = true;
+// select the button and store into variable
+let button = document.querySelector(".reset");
+// select divs for our reset function
+let box = document.querySelectorAll("div");
+function placeMark(elem) {
+	if (toggle === true) {
+		elem.innerHTML = "X";
+		toggle = false;
+	} else {
+		elem.innerHTML = "O";
+		toggle = true;
+	}
 }
-function changeMarkerToO() {
-	var gameMarker = "0";
-	console.log("The x button was clicked!");
-}
-function placeMark() {
-	console.log("dsf");
-}
+// add event listener for click to run function
+button.addEventListener("click", function() {
+	// iterate through all divs
+	for (var i = 0; i < box.length; i++) {
+		// set innerHTML to be empty with each iteration
+		box[i].innerHTML = "";
+	}
+});
